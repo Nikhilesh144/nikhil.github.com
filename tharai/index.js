47c7bbeb9d2d80b4fc1854c1bba487c0f701680b
Nikhilesh144 <121549469+Nikhilesh144@users.noreply.github.com>
2024-04-29 00:14:24 +0530

    edit3

---
+++ tharai/index.js
@@ -0,0 +1,8 @@
+function show(){
+    const s=document.querySelector('.sidebar')
+    s.style.display='flex';
+}
+function hide(){
+    const s=document.querySelector('.sidebar')
+    s.style.display='none';
+}
